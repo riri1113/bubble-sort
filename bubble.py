f2dfbc89e909cc430f665cd8f38f0df225e24759
skawashin1122 <941574_teacher@g.miyazaki-c.ed.jp>
2025-06-18 22:13:38 +0900

    バブルソートのテスト問題

---
+++ bubble.py
@@ -0,0 +1,16 @@
+# 配列Suにデータを記憶する（データ件数はn件）
+Su = ["peach", "melon", "apple", "grape", "strawberry", "banana", "orange", "litchi", "kiwi", "pineapple"]
+n = len(Su)
+print(f"元の配列: {Su}")
+print(f"データ件数：{n}件")
+
+# バブルソートの実行
+for g in range(_____, 0, -1):
+    for j in range(0, ___, 1):
+        if Su[j] ___ Su[j + 1]:
+            Hozon = Su[j]
+            Su[j] = _____
+            Su[j + 1] = Hozon
+
+for k in range(___):
+    print(f"Su({k}): {Su[k]}")
